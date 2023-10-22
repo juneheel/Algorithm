68dfa613e0d77c94f2bc535dd13f18cd1f10e081
JUNI <131724311+juneheel@users.noreply.github.com>
2023-10-22 13:14:03 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.94 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -3,8 +3,6 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] wallpaper) {
       
-        int[][] screen = new int[wallpaper.length+1][wallpaper[0].length()+1];
-        
         int xMin=50,yMin=50,xMax=0,yMax=0;
         int i=0;
         
@@ -15,25 +13,18 @@ class Solution {
                     System.out.printf("%d %d ",i,j); // i=1 j=0
                     
                         if(i<xMin){
-                            
                             xMin = i;
                         }
                         if(i+1>xMax){
-                            
                             xMax = i+1;
                         }
-                    
-                    
                         if(j<yMin){
-                            
                             yMin = j;
                         }
                         if(j+1>yMax){
-                           
                             yMax = j+1;
                         }
                     }
-                
                 else{
                     continue;
                 }

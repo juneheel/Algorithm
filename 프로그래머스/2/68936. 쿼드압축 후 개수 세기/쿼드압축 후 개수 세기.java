fa313d73e1ad6a437b3e260e496ad7b73c60effc
JUNI <131724311+juneheel@users.noreply.github.com>
2023-12-14 03:39:52 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 2.93 ms, Memory: 95.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -0,0 +1,27 @@
+class Solution {
+    static int[] answer = new int[2];
+    public int[] solution(int[][] arr) {
+        zip(0,0,arr.length,arr);
+        return answer;
+    }
+    public static void zip(int x, int y,int r,int[][] arr){
+        int compare = arr[x][y];
+        outerLoop:
+        {
+            for(int i=x;i<x+r;i++){
+                    for(int j=y;j<y+r;j++){
+                        if(compare != arr[i][j]){
+                            break outerLoop;
+                        }   
+                    }
+            }
+            answer[arr[x][y]]++;
+            return ;
+        }
+        int half = r/2;
+        zip(x,y,half,arr);
+        zip(x+half,y,half,arr);
+        zip(x,y+half,half,arr);
+        zip(x+half,y+half,half,arr);
+    }
+}

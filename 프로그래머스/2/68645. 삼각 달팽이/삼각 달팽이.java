d919fc9d351f50b771544800e4027d4eb539af58
JUNI <131724311+juneheel@users.noreply.github.com>
2023-12-23 18:52:50 +0900

    [level 2] Title: 삼각 달팽이, Time: 43.44 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,39 +1,30 @@
 import java.util.*;
 class Solution {
+    
+    private static final int[] dx = {0,1,-1};
+    private static final int[] dy = {1,0,-1};
+    
     public int[] solution(int n) {
         int[][] triangle = new int[n][n];
         List<Integer> answerList = new ArrayList<>();
         
         int x = 0;
         int y = 0;
+        int d = 0;
         int state = 1; // 현재숫자를 나타냄
         int finish = cal(n);
         
-        for(int i=1;i<=finish;i++){
-            while(true){ // 1차행동 : 아래로이동
+        while(state<=finish){
              triangle[y][x] = state++;
-                if(y+1 ==n||triangle[y+1][x] != 0) break;
-                y++;
+             int nx = x+dx[d];
+             int ny = y+dy[d];
+            if(nx == n||ny==n||nx==-1||ny==-1||triangle[ny][nx]!=0){
+                d = (d+1)%3;
+                nx = x+dx[d];
+                ny = y+dy[d];
             }
-            if(x+1==n||triangle[y][x+1]!=0) break; // 내려가서끝인경우
-            x++;
-            while(true){ // 2차행동 : 오른쪽으로 이동
-                triangle[y][x] = state++;
-                if(x+1==n||triangle[y][x+1]!=0) break;
-                x++;
-            }
-            if(triangle[y-1][x-1]!=0) break; // 오른쪽으로 이동해서 끝인경우
-            y--;
-            x--;
-            
-            while(true){
-                triangle[y][x]=state++;    
-                if(triangle[y-1][x-1]!=0) break;
-                y--;
-                x--;
-            }
-            if(y+1==n||triangle[y+1][x]!=0)break;
-            y++;
+            x = nx;
+            y = ny;
         }
             
         for(int[] as : triangle){
